df94af829115dedff6a58ad774f11a7d01685aa6
jjavimu <jmulero@ucm.es>
2022-04-20 17:26:26 +0200

    ipfs ok

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
 
 import fetch from "node-fetch";
-/*
+import schedule from "node-schedule";
+
+// https://crontab.guru/
+// https://javascript.plainenglish.io/scheduling-tasks-on-heroku-ebfa63c124dd
+// var j = schedule.scheduleJob('* * * * * *', function () {
+//     console.log('The answer to life, the universe, and everything!');
+// });
+
+
 const response = await fetch('https://precariedapp.herokuapp.com/get', {
     method: "GET"
 })
-    .then(res => res.json()).then(res => console.log(res));
-*/
+    .then(res => res.json())
+    .then(res => {
+        console.log(res);
+        // add(res);
+        // del(res);
+    }
+    );
 
+async function del(res) {
+    await fetch("https://cache-twitter.herokuapp.com/delete", {
+        method: "DELETE"
+    })
+        .catch(error => {
+            console.log(error);
+            return;
+        });
+}
 
-const prueba = "[{\"link\":\"https://twitter.com/Mari196222/status/1513231827821027334\",\"id\":1513231827821027300,\"text\":\"yomerebelo10a yomerebelo10a no al acoso mediático adoctrinamiento humillaciones vulnerar dchos tramas policiales apagón cloaca lfdlt al lío\",\"user\":\"Mari196222\",\"date\":1649613956,\"likes\":0,\"retweets\":0,\"replies\":0,\"hashtags\":[\"YoMeRebelo10A\",\"YoMeRebelo10A\"]},{\"link\":\"https://twitter.com/Mari196222/status/1513232118029107202\",\"id\":1513232118029107200,\"text\":\"yomerebelo10a yomerebelo10a no al acoso mediático adoctrinamiento humillaciones vulnerar dchos tramas policiales apagón cloaca lfdlt al lío\",\"user\":\"Mari196222\",\"date\":1649614026,\"likes\":0,\"retweets\":0,\"replies\":0,\"hashtags\":[\"YoMeRebelo10A\",\"YoMeRebelo10A\"]}]";
-console.log(JSON.stringify(JSON.parse(prueba)));
-
-// await fetch("https://cache-twitter.herokuapp.com/delete", {
-//     method: "DELETE"
-// })
-//     .catch(error => {
-//         console.log(error);
-//         return;
-//     });
-
-
-await fetch("https://cache-twitter.herokuapp.com/add", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify(JSON.parse(prueba)),
-})
-    .catch(error => {
-        console.log(error);
-        return;
-    });
-
+async function add(res) {
+    await fetch("https://cache-twitter.herokuapp.com/add", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: res,
+    })
+        .catch(error => {
+            console.log(error);
+            return;
+        });
+}
