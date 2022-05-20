83a1896774d8b76a061b0d1372875ed56347fc74
jjavimu <jmulero@ucm.es>
2022-05-20 14:00:52 +0200

    comment funct for heroku limit

--- index.js
+++ index.js
@@ -4,28 +4,28 @@ import schedule from "node-schedule";
 
 
 // // Schedule: at minute 0 past hour 12 and 0.
-var j = schedule.scheduleJob('0 0 0,12 * * *', async function () {
-    await fetch('https://precariedappv2.herokuapp.com/get', { method: "GET" })
-        .then(handleErrors) // Handle HTTP errors
-        .then(res => res.json()) // Convert to JSON response
-        .then(res => checkFormat(res)) // Check format
-        .then(tweets => { // Process the JSON string
-            del() // Delete all old tweets
-                .then(handleErrors) // Handle HTTP errors
-                .then(deleteRes => deleteRes.json()) // Convert to JSON response
-                .then(deleteData => {  // Process the JSON response
-                    console.log("Tweets deleted:", deleteData.deletedCount);
-                    add(tweets)
-                        .then(handleErrors) // Handle HTTP errors
-                        .then(addRes => addRes.json()) // Convert to JSON response
-                        .then(addData => console.log("Tweets added:", addData.insertedCount)) // Process the JSON response
-                        .catch(err => console.log("Function add() failed.", err)); // Show errors
-                })
-                .catch(err => console.log("Function del() failed.", err)); // Show errors
-        }
-        )
-        .catch(err => console.log("Function fetch() failed.", err)); // Show errors
-});
+// var j = schedule.scheduleJob('0 0 0,12 * * *', async function () {
+//     await fetch('https://precariedappv2.herokuapp.com/get', { method: "GET" })
+//         .then(handleErrors) // Handle HTTP errors
+//         .then(res => res.json()) // Convert to JSON response
+//         .then(res => checkFormat(res)) // Check format
+//         .then(tweets => { // Process the JSON string
+//             del() // Delete all old tweets
+//                 .then(handleErrors) // Handle HTTP errors
+//                 .then(deleteRes => deleteRes.json()) // Convert to JSON response
+//                 .then(deleteData => {  // Process the JSON response
+//                     console.log("Tweets deleted:", deleteData.deletedCount);
+//                     add(tweets)
+//                         .then(handleErrors) // Handle HTTP errors
+//                         .then(addRes => addRes.json()) // Convert to JSON response
+//                         .then(addData => console.log("Tweets added:", addData.insertedCount)) // Process the JSON response
+//                         .catch(err => console.log("Function add() failed.", err)); // Show errors
+//                 })
+//                 .catch(err => console.log("Function del() failed.", err)); // Show errors
+//         }
+//         )
+//         .catch(err => console.log("Function fetch() failed.", err)); // Show errors
+// });
 
 function checkFormat(res) {
     // Check format (only extra commas)
